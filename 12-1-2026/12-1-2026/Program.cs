cf87f565b2dcfc33422ec67a0296a33f898a1d08
asharmousa <asharmousa2@gmail.com>
2026-01-12 13:30:35 +0300

    c#/12-1-2026

---
+++ 12-1-2026/12-1-2026/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_1_2026
+{
+    class Bank
+
+    {
+        //5 proparities
+        private string coustomer_name;
+        private string coustomer_id;
+        private string coustomer_password;
+        private string coustomer_phone;
+        private double balance;
+
+
+        //getter and setter 
+        public string Coustomer_Name
+            {
+              get{ return coustomer_name; }
+              set{ coustomer_name = value; }
+            }
+        public string Coustomer_Id
+        {
+            get { return coustomer_id; }
+            set { coustomer_id = value; }
+        }
+        public string Coustomer_Password
+        {
+            get { return coustomer_password; }
+            set { coustomer_password = value; }
+        }
+        public string Coustomer_phone
+        {
+            get { return coustomer_phone; }
+            set { coustomer_phone = value; }
+        }
+       
+        public double Balance
+        {
+            get { return balance; }
+            set { balance = value; }
+        }
+        public Bank(string name, string id, string password, string phone , double balance)
+        {
+            this.coustomer_name = name;
+            this.coustomer_id = id;
+            this.coustomer_password = password;
+            this.coustomer_phone = phone;
+            this.balance = balance;
+        }
+      
+
+        //3 methodes 
+        //public void Create_acount ()
+        //{
+            
+        //    Console.Write("Enter your name : "+coustomer_name);
+        //    Console.ReadLine();
+        //    Console.WriteLine();
+        //    Console.Write("Enter your password : " + coustomer_password);
+        //    Console.ReadLine();
+        //    Console.WriteLine();
+        //    Console.Write("Enter your phone : " + coustomer_phone);
+        //    Console.ReadLine();
+        //    Console.WriteLine();
+        //    balance = 0;
+
+        //}
+
+        public void CheckBalance()
+        {
+            Console.WriteLine("in your account you have :  " + balance + "JD");
+        }
+       public void Deposit(int amount)
+        {
+
+            balance+= amount;
+            Console.WriteLine("Your current amount is :" + balance+" JD");
+
+        }
+
+        public void Withdraw(double amount)
+        {
+            Console.WriteLine();
+            if (amount % 5 != 0)
+            { 
+                Console.WriteLine("Withdrawal amount must be a multiple of 5."); 
+            } 
+            else if (amount > balance) 
+            { 
+                Console.WriteLine("Insufficient funds. Your balance is: " + balance + " JD"); 
+            } 
+            else
+            { 
+                balance -= amount; 
+                Console.WriteLine(amount + " JD withdrawn. New balance: " + balance + " JD"); 
+            } 
+        }
+        ~Bank()
+        {
+            Console.WriteLine("obj deleted");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank account = new Bank("Ashar", "C123", "pass123", "0799999999", 100);
+            account.CheckBalance();
+            account.Deposit(50); 
+            account.Withdraw(55); 
+            account.Withdraw(200); 
+        }
+    }
+}
---
+++ 12-1-2026/12-1-2026/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("12-1-2026")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("12-1-2026")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9c374525-564f-41ef-92b8-593e913be6a3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
